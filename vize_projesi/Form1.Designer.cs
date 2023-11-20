a24b74fd36ae39a55283bf466b8d18644d58a1f1
CyberWarmonger <114314744+CyberWarmonger@users.noreply.github.com>
2023-11-20 20:36:13 +0300

    Add files via upload

---
+++ vize_projesi/Form1.Designer.cs
@@ -0,0 +1,374 @@
+﻿namespace vize_projesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxKunefe = new System.Windows.Forms.TextBox();
+            this.textBoxTrilece = new System.Windows.Forms.TextBox();
+            this.textBoxCigKofte = new System.Windows.Forms.TextBox();
+            this.textBoxDoner = new System.Windows.Forms.TextBox();
+            this.textBoxIskender = new System.Windows.Forms.TextBox();
+            this.textBoxTost = new System.Windows.Forms.TextBox();
+            this.textBoxCay = new System.Windows.Forms.TextBox();
+            this.textBoxFanta = new System.Windows.Forms.TextBox();
+            this.textBoxSu = new System.Windows.Forms.TextBox();
+            this.textBoxKola = new System.Windows.Forms.TextBox();
+            this.checkBoxKunefe = new System.Windows.Forms.CheckBox();
+            this.checkBoxTrilece = new System.Windows.Forms.CheckBox();
+            this.checkBoxCıgKofte = new System.Windows.Forms.CheckBox();
+            this.checkBoxDoner = new System.Windows.Forms.CheckBox();
+            this.checkBoxIskender = new System.Windows.Forms.CheckBox();
+            this.checkBoxTost = new System.Windows.Forms.CheckBox();
+            this.checkBoxCay = new System.Windows.Forms.CheckBox();
+            this.checkBoxFanta = new System.Windows.Forms.CheckBox();
+            this.checkBoxSu = new System.Windows.Forms.CheckBox();
+            this.checkBoxKola = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxKunefe);
+            this.groupBox1.Controls.Add(this.textBoxTrilece);
+            this.groupBox1.Controls.Add(this.textBoxCigKofte);
+            this.groupBox1.Controls.Add(this.textBoxDoner);
+            this.groupBox1.Controls.Add(this.textBoxIskender);
+            this.groupBox1.Controls.Add(this.textBoxTost);
+            this.groupBox1.Controls.Add(this.textBoxCay);
+            this.groupBox1.Controls.Add(this.textBoxFanta);
+            this.groupBox1.Controls.Add(this.textBoxSu);
+            this.groupBox1.Controls.Add(this.textBoxKola);
+            this.groupBox1.Controls.Add(this.checkBoxKunefe);
+            this.groupBox1.Controls.Add(this.checkBoxTrilece);
+            this.groupBox1.Controls.Add(this.checkBoxCıgKofte);
+            this.groupBox1.Controls.Add(this.checkBoxDoner);
+            this.groupBox1.Controls.Add(this.checkBoxIskender);
+            this.groupBox1.Controls.Add(this.checkBoxTost);
+            this.groupBox1.Controls.Add(this.checkBoxCay);
+            this.groupBox1.Controls.Add(this.checkBoxFanta);
+            this.groupBox1.Controls.Add(this.checkBoxSu);
+            this.groupBox1.Controls.Add(this.checkBoxKola);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(13, 12);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox1.Size = new System.Drawing.Size(261, 708);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Masa1";
+            // 
+            // textBoxKunefe
+            // 
+            this.textBoxKunefe.Location = new System.Drawing.Point(173, 622);
+            this.textBoxKunefe.Name = "textBoxKunefe";
+            this.textBoxKunefe.Size = new System.Drawing.Size(54, 24);
+            this.textBoxKunefe.TabIndex = 23;
+            // 
+            // textBoxTrilece
+            // 
+            this.textBoxTrilece.Location = new System.Drawing.Point(173, 582);
+            this.textBoxTrilece.Name = "textBoxTrilece";
+            this.textBoxTrilece.Size = new System.Drawing.Size(54, 24);
+            this.textBoxTrilece.TabIndex = 22;
+            // 
+            // textBoxCigKofte
+            // 
+            this.textBoxCigKofte.Location = new System.Drawing.Point(173, 471);
+            this.textBoxCigKofte.Name = "textBoxCigKofte";
+            this.textBoxCigKofte.Size = new System.Drawing.Size(54, 24);
+            this.textBoxCigKofte.TabIndex = 21;
+            // 
+            // textBoxDoner
+            // 
+            this.textBoxDoner.Location = new System.Drawing.Point(173, 427);
+            this.textBoxDoner.Name = "textBoxDoner";
+            this.textBoxDoner.Size = new System.Drawing.Size(54, 24);
+            this.textBoxDoner.TabIndex = 20;
+            // 
+            // textBoxIskender
+            // 
+            this.textBoxIskender.Location = new System.Drawing.Point(173, 382);
+            this.textBoxIskender.Name = "textBoxIskender";
+            this.textBoxIskender.Size = new System.Drawing.Size(54, 24);
+            this.textBoxIskender.TabIndex = 19;
+            // 
+            // textBoxTost
+            // 
+            this.textBoxTost.Location = new System.Drawing.Point(173, 337);
+            this.textBoxTost.Name = "textBoxTost";
+            this.textBoxTost.Size = new System.Drawing.Size(54, 24);
+            this.textBoxTost.TabIndex = 18;
+            // 
+            // textBoxCay
+            // 
+            this.textBoxCay.Location = new System.Drawing.Point(173, 220);
+            this.textBoxCay.Name = "textBoxCay";
+            this.textBoxCay.Size = new System.Drawing.Size(54, 24);
+            this.textBoxCay.TabIndex = 17;
+            // 
+            // textBoxFanta
+            // 
+            this.textBoxFanta.Location = new System.Drawing.Point(173, 173);
+            this.textBoxFanta.Name = "textBoxFanta";
+            this.textBoxFanta.Size = new System.Drawing.Size(54, 24);
+            this.textBoxFanta.TabIndex = 16;
+            // 
+            // textBoxSu
+            // 
+            this.textBoxSu.Location = new System.Drawing.Point(173, 128);
+            this.textBoxSu.Name = "textBoxSu";
+            this.textBoxSu.Size = new System.Drawing.Size(54, 24);
+            this.textBoxSu.TabIndex = 15;
+            // 
+            // textBoxKola
+            // 
+            this.textBoxKola.Location = new System.Drawing.Point(173, 84);
+            this.textBoxKola.Name = "textBoxKola";
+            this.textBoxKola.Size = new System.Drawing.Size(54, 24);
+            this.textBoxKola.TabIndex = 14;
+            // 
+            // checkBoxKunefe
+            // 
+            this.checkBoxKunefe.AutoSize = true;
+            this.checkBoxKunefe.Location = new System.Drawing.Point(18, 624);
+            this.checkBoxKunefe.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.checkBoxKunefe.Name = "checkBoxKunefe";
+            this.checkBoxKunefe.Size = new System.Drawing.Size(82, 22);
+            this.checkBoxKunefe.TabIndex = 13;
+            this.checkBoxKunefe.Text = "Künefe";
+            this.checkBoxKunefe.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxTrilece
+            // 
+            this.checkBoxTrilece.AutoSize = true;
+            this.checkBoxTrilece.Location = new System.Drawing.Point(18, 582);
+            this.checkBoxTrilece.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.checkBoxTrilece.Name = "checkBoxTrilece";
+            this.checkBoxTrilece.Size = new System.Drawing.Size(81, 22);
+            this.checkBoxTrilece.TabIndex = 12;
+            this.checkBoxTrilece.Text = "Trileçe";
+            this.checkBoxTrilece.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxCıgKofte
+            // 
+            this.checkBoxCıgKofte.AutoSize = true;
+            this.checkBoxCıgKofte.Location = new System.Drawing.Point(18, 471);
+            this.checkBoxCıgKofte.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.checkBoxCıgKofte.Name = "checkBoxCıgKofte";
+            this.checkBoxCıgKofte.Size = new System.Drawing.Size(100, 22);
+            this.checkBoxCıgKofte.TabIndex = 11;
+            this.checkBoxCıgKofte.Text = "Çiğ Köfte";
+            this.checkBoxCıgKofte.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxDoner
+            // 
+            this.checkBoxDoner.AutoSize = true;
+            this.checkBoxDoner.Location = new System.Drawing.Point(19, 429);
+            this.checkBoxDoner.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.checkBoxDoner.Name = "checkBoxDoner";
+            this.checkBoxDoner.Size = new System.Drawing.Size(76, 22);
+            this.checkBoxDoner.TabIndex = 10;
+            this.checkBoxDoner.Text = "Döner";
+            this.checkBoxDoner.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxIskender
+            // 
+            this.checkBoxIskender.AutoSize = true;
+            this.checkBoxIskender.Location = new System.Drawing.Point(18, 382);
+            this.checkBoxIskender.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.checkBoxIskender.Name = "checkBoxIskender";
+            this.checkBoxIskender.Size = new System.Drawing.Size(94, 22);
+            this.checkBoxIskender.TabIndex = 9;
+            this.checkBoxIskender.Text = "İskender";
+            this.checkBoxIskender.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxTost
+            // 
+            this.checkBoxTost.AutoSize = true;
+            this.checkBoxTost.Location = new System.Drawing.Point(19, 339);
+            this.checkBoxTost.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.checkBoxTost.Name = "checkBoxTost";
+            this.checkBoxTost.Size = new System.Drawing.Size(64, 22);
+            this.checkBoxTost.TabIndex = 8;
+            this.checkBoxTost.Text = "Tost";
+            this.checkBoxTost.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxCay
+            // 
+            this.checkBoxCay.AutoSize = true;
+            this.checkBoxCay.Location = new System.Drawing.Point(18, 220);
+            this.checkBoxCay.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.checkBoxCay.Name = "checkBoxCay";
+            this.checkBoxCay.Size = new System.Drawing.Size(59, 22);
+            this.checkBoxCay.TabIndex = 7;
+            this.checkBoxCay.Text = "Çay";
+            this.checkBoxCay.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxFanta
+            // 
+            this.checkBoxFanta.AutoSize = true;
+            this.checkBoxFanta.Location = new System.Drawing.Point(18, 173);
+            this.checkBoxFanta.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.checkBoxFanta.Name = "checkBoxFanta";
+            this.checkBoxFanta.Size = new System.Drawing.Size(72, 22);
+            this.checkBoxFanta.TabIndex = 6;
+            this.checkBoxFanta.Text = "Fanta";
+            this.checkBoxFanta.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxSu
+            // 
+            this.checkBoxSu.AutoSize = true;
+            this.checkBoxSu.Location = new System.Drawing.Point(18, 128);
+            this.checkBoxSu.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.checkBoxSu.Name = "checkBoxSu";
+            this.checkBoxSu.Size = new System.Drawing.Size(50, 22);
+            this.checkBoxSu.TabIndex = 5;
+            this.checkBoxSu.Text = "Su";
+            this.checkBoxSu.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxKola
+            // 
+            this.checkBoxKola.AutoSize = true;
+            this.checkBoxKola.Location = new System.Drawing.Point(19, 84);
+            this.checkBoxKola.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.checkBoxKola.Name = "checkBoxKola";
+            this.checkBoxKola.Size = new System.Drawing.Size(64, 22);
+            this.checkBoxKola.TabIndex = 4;
+            this.checkBoxKola.Text = "Kola";
+            this.checkBoxKola.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(8, 529);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "TATLILAR";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(8, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "İÇECEKLER";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(8, 285);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "YEMEKLER";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(295, 316);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Onayla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(648, 338);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(345, 292);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1097, 755);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBoxKunefe;
+        private System.Windows.Forms.CheckBox checkBoxTrilece;
+        private System.Windows.Forms.CheckBox checkBoxCıgKofte;
+        private System.Windows.Forms.CheckBox checkBoxDoner;
+        private System.Windows.Forms.CheckBox checkBoxIskender;
+        private System.Windows.Forms.CheckBox checkBoxTost;
+        private System.Windows.Forms.CheckBox checkBoxCay;
+        private System.Windows.Forms.CheckBox checkBoxFanta;
+        private System.Windows.Forms.CheckBox checkBoxSu;
+        private System.Windows.Forms.CheckBox checkBoxKola;
+        private System.Windows.Forms.TextBox textBoxKunefe;
+        private System.Windows.Forms.TextBox textBoxTrilece;
+        private System.Windows.Forms.TextBox textBoxCigKofte;
+        private System.Windows.Forms.TextBox textBoxDoner;
+        private System.Windows.Forms.TextBox textBoxIskender;
+        private System.Windows.Forms.TextBox textBoxTost;
+        private System.Windows.Forms.TextBox textBoxCay;
+        private System.Windows.Forms.TextBox textBoxFanta;
+        private System.Windows.Forms.TextBox textBoxSu;
+        private System.Windows.Forms.TextBox textBoxKola;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ vize_projesi/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vize_projesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, decimal> urunFiyatlari = new Dictionary<string, decimal>();
+            urunFiyatlari.Add("Kola", 5.0m);
+            urunFiyatlari.Add("gazoz", 3.0m);
+            urunFiyatlari.Add("çay", 1.5m);
+            urunFiyatlari.Add("kahve", 1.5m);
+            urunFiyatlari.Add("çay", 1.5m);
+            urunFiyatlari.Add("iskender", 1.5m);
+            urunFiyatlari.Add("makarna", 1.5m);
+            urunFiyatlari.Add("döner", 1.5m);
+            urunFiyatlari.Add("lahmacun", 1.5m);
+            urunFiyatlari.Add("künefe", 1.5m);
+            urunFiyatlari.Add("pasta", 1.5m);
+            // Diğer ürünlerin fiyatlarını da ekleyin
+
+            // Ürünlerin adetlerini tutacak sözlük
+            Dictionary<string, int> urunAdetleri = new Dictionary<string, int>();
+            urunAdetleri.Add("Kola", 0);
+            urunAdetleri.Add("Ayran", 0);
+            urunAdetleri.Add("kahve", 0);
+            urunAdetleri.Add("çay", 0);
+            urunFiyatlari.Add("iskender", 0);
+            urunFiyatlari.Add("makarna", 0);
+            urunFiyatlari.Add("döner", 0);
+            urunFiyatlari.Add("lahmacun", 0);
+            urunFiyatlari.Add("künefe", 0);
+            urunFiyatlari.Add("pasta", 0);
+            // Diğer ürünlerin adetlerini de ekleyin
+
+            // Kullanıcının seçtiği ürünlerin fiyatlarını ve adetlerini toplamak için değişkenler
+            decimal toplamFiyat = 0;
+
+            // Kullanıcının seçtiği ürünleri kontrol edin ve fiyatları ve adetleri toplayın
+            if (checkBoxKola.Checked)
+            {
+                int adet = Convert.ToInt32(textBoxKola.Text); // Kola için adet girişi yapılacak bir TextBox (txtKolaAdet) kullanıldığını varsayalım
+                toplamFiyat += urunFiyatlari["Kola"] * adet;
+                urunAdetleri["Kola"] = adet;
+            }
+
+            if (checkBoxSu.Checked)
+            {
+                int adet = Convert.ToInt32(textBoxSu.Text); // Ayran için adet girişi yapılacak bir TextBox (txtAyranAdet) kullanıldığını varsayalım
+                toplamFiyat += urunFiyatlari["gazoz"] * adet;
+                urunAdetleri["gazoz"] = adet;
+            }
+
+            if (checkBoxFanta.Checked)
+            {
+                int adet = Convert.ToInt32(textBoxFanta.Text); // Su için adet girişi yapılacak bir TextBox (txtSuAdet) kullanıldığını varsayalım
+                toplamFiyat += urunFiyatlari["çay"] * adet;
+                urunAdetleri["çay"] = adet;
+            }
+            if (checkBoxFanta.Checked)
+            {
+                int adet = Convert.ToInt32(textBoxFanta.Text); // Su için adet girişi yapılacak bir TextBox (txtSuAdet) kullanıldığını varsayalım
+                toplamFiyat += urunFiyatlari["kahve"] * adet;
+                urunAdetleri["kahve"] = adet;
+            }
+            if (checkBoxCay.Checked)
+            {
+                int adet = Convert.ToInt32(textBoxCay.Text); // Su için adet girişi yapılacak bir TextBox (txtSuAdet) kullanıldığını varsayalım
+                toplamFiyat += urunFiyatlari["iskender"] * adet;
+                urunAdetleri["iskender"] = adet;
+            }
+
+
+            // Diğer ürünlerin seçili olup olmadığını ve adetlerini kontrol edin ve fiyatları ve adetleri toplayın
+            // Aynı yöntemi diğer grup kutuları, checkbox'lar ve adet girişleri için de kullanabilirsiniz
+
+            // Toplam fiyatı ve seçilen ürünleri göstermek için bir label veya başka bir kontrol kullanabilirsiniz
+            label1.Text = $"Toplam Fiyat: {toplamFiyat} TL";
+            // Ayrıca, seçilen ürünlerin adetlerini de kullanarak başka bir şekilde gösterebilirsiniz
+            // Örneğin, her bir ürünün adetini ve ismini listeleyebilirsiniz
+            foreach (var urun in urunAdetleri)
+            {
+                listBox1.Items.Add($"{urun.Key}: {urun.Value} adet");
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
